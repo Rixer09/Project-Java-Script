b8f3af13f9fcf35ebcd24462c20118f00c0a8193
Rixer09 <154397271+Rixer09@users.noreply.github.com>
2025-07-14 19:29:38 +0000

    Variable Understandind

---
+++ Variable_File/test1.js
@@ -0,0 +1,12 @@
+/*This File is to know about the use of variable Declaration, Types , what and How they should be declared */
+const myName1= "Shivam Parmar"
+let myName2="Ram Parmar"
+var myName3="Lakshman Parmar"
+myName3="Hanuman"/*the [myName is called Camel typing also it is used by most also
+my_name is Kabab typing gathering some attention is being usedby lot but then also Camel typing is by default]*/
+//Now checking the properties of variables
+//myName1="Ram"// will give an error becz the the variable declared is constant and the value contained cannot be changed
+// myName2="Shivam Parmar" // will change 
+// myName3="Shiva" // will change
+console.table([myName1 , myName2 , myName3])
+// to run the program type in terminal $ node Variable_File/test1.js
