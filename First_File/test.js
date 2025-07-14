ec74532693070fcc4e5071997707d54e6f60ff3d
Rixer09 <154397271+Rixer09@users.noreply.github.com>
2025-07-14 19:10:29 +0000

    Added some more info about console...

--- First_File/test.js
+++ First_File/test.js
@@ -1,3 +1,7 @@
 console.log("Shivam_Parmar")
 console.log("I now Know how Console.log works and what it do")
 console.log("To sum it up it is a prnting statement")
+/*In order to print number of variables in a go and print them in a form of table then this syntax is used.
+Also this is multi line comment
+*/
+console.table([])
