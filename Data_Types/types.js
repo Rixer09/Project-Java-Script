cc59df90171b6b279efed61dc2f859a0fd27c5f2
Rixer09 <154397271+Rixer09@users.noreply.github.com>
2025-07-15 09:20:06 +0000

    Data_Types

---
+++ Data_Types/types.js
@@ -0,0 +1,18 @@
+/*In order to become a genuine devloper or coder ensure that your program 
+is well structured , well intialized , and well designed.In short myst be readable to others*/
+
+//there are 4 types of data types
+//1. number>> intializes number variables to store digits. bigint is used to to intializes variable having long string of digits
+let didgi=2345
+//2. string>> intializes string variables to store words 
+let name="Shivam Parmar"
+//3. bollean>> intializes variables containing either True or False only
+let boo=true
+//4. symbol >> used to declare a variable unique from others, mostly used in React and Typescript
+// 5. null >> means the variable declared is not having any value
+let k=null
+//6. undefined >> means the variable declared is not defined
+let value;// error stating the value is undefined 
+console.log(typeof "Shivam Parmar" ) //, "Shivam Parmar" , null , 23450
+
+//null falls under the category of object i.e typeof null>> object ,but typeof undefined>> undefined
