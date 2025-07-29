1c712fe5f3cd6c7f1748b4d0416002b9fc0ced3d
Rixer09 <154397271+Rixer09@users.noreply.github.com>
2025-07-29 17:51:31 +0000

    Conversion+Operations

--- DataType_Conversion/type.js
+++ DataType_Conversion/type.js
@@ -17,3 +17,23 @@ console.log(value2)//But here the value2 is NAN i.e not a number. Therefore doub
 /* Now likewise Number() function there are other function like Boolean()=>converts into True or False
 String()=> converts into string and more , search when in use*/
 
+
+//Operations
+/* Simple operations are 1+2=>3 , 1*2=>2 , 1%2=>0 , 1/2=>0 , 1**2=>2(1**2 means 1 is raised to the power 2)
+ and other mathematical operations like prefix and postfix addition [++a , a++] */
+// real confusing operations are like String + Number.
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 3 + "2");// Here number is Priortized
+console.log("1" + 2 + 2);// Note : the first data type will dominate. Here String is priortized
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
